1c89a442223ee0b19b372e4568429df533ebacb4
RapidBlaze53 <127673703+RapidBlaze53@users.noreply.github.com>
2023-09-27 20:35:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function intro() {
+    window.location = "intro.html";
+    console.log("intro")
+}
+
+function dev() {
+    window.location = "dev.html";
+    console.log("dev")
+}
+
+function conc() {
+    window.location = "conc.html"
+    console.log("conclusion")
+}
+
+function home() {
+    window.location = "index.html"
+    console.log("home")
+}
